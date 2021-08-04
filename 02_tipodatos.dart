fd66d38c7f6f003a3fb195c9073b8c92fbeeb888
John Doe <simbron_malpica@hotmail.com>
2021-08-04 09:17:55 -0500

    concatena dos variable

--- 02_tipodatos.dart
+++ 02_tipodatos.dart
@@ -1,13 +1,13 @@
 void main() {
-  
+
   int a = 10;
   double b = 11.1;
   int? c;
 
   print(c);
 
-  String nombre = 'Adolfo';
-  String nombre2 = "Adolfo";
-  print(nombre == nombre2);
+  String nombre = 'Adolfo ';
+  String saludo = "hola ingeniero";
+  print(nombre + saludo);
 
 }
